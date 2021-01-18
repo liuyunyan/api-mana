be98df250caffd335b3b95f2e9e3be98c1756c7e
yunyan <liuyunyna@163.com>
2021-01-18 19:15:53 +0800

    fix:login

--- src/config.js
+++ src/config.js
@@ -1,44 +1,46 @@
-
-let mockServer = "http://39.105.90.208:3000/"
-let serverUrl = `${mockServer}mock/6`;
-
+let mockServer = "http://39.105.90.208:3000/";
+let serverUrl =
+  process.env.NODE_ENV === "development"
+    ? `${mockServer}mock/6`
+    : "http://39.105.90.208:5418";
+// console.log(process.env.PUBLIC_URL);
 const Config = {
-    serverUrl,
-    source: {
-        query: `${serverUrl}/datasource/list`,
-        add: `${serverUrl}/datasource/add`,
-        queryDetail: `${serverUrl}/datasource/`,
-        update: `${serverUrl}/datasource`,
-        delete: `${serverUrl}/datasource/`,
-        connection: `${serverUrl}/datasource/connection`,
-    },
-    third: {
-        query: `${serverUrl}/thirdPartyPlatform/list`,
-        add: `${serverUrl}/thirdPartyPlatform/add`,
-        queryDetail: `${serverUrl}/thirdPartyPlatform/`,
-        update: `${serverUrl}/thirdPartyPlatform`,
-        delete: `${serverUrl}/thirdPartyPlatform/`,
-        generateAPIKey: `${serverUrl}/thirdPartyPlatform/generateAPIKey`,
-    },
-    user: {
-        login:`${serverUrl}/user/login`,
-        query: `${serverUrl}/user/list`,
-        add: `${serverUrl}/user/add`,
-        queryDetail: `${serverUrl}/user/`,
-        update: `${serverUrl}/user`,
-        delete: `${serverUrl}/user/`,
-        connection: `${serverUrl}/user/connection`,
-    },
-    interface: {
-        query: `${serverUrl}/external/interface/list`,
-        add: `${serverUrl}/external/interface/add`,
-        queryDetail: `${serverUrl}/external/interface/`,
-        update: `${serverUrl}/external/interface`,
-        delete: `${serverUrl}/external/interface/`,
-        queryDBs: `${serverUrl}/external/listDatabases/`,
-        queryDBTables: `${serverUrl}/external/listTables/`,
-        queryDBFileds: `${serverUrl}/external/getTableSchema/`,
-        publish: `${serverUrl}external/interface/publish/`,
-    },
+  serverUrl,
+  source: {
+    query: `${serverUrl}/datasource/list`,
+    add: `${serverUrl}/datasource/add`,
+    queryDetail: `${serverUrl}/datasource/`,
+    update: `${serverUrl}/datasource`,
+    delete: `${serverUrl}/datasource/`,
+    connection: `${serverUrl}/datasource/connection`,
+  },
+  third: {
+    query: `${serverUrl}/thirdPartyPlatform/list`,
+    add: `${serverUrl}/thirdPartyPlatform/add`,
+    queryDetail: `${serverUrl}/thirdPartyPlatform/`,
+    update: `${serverUrl}/thirdPartyPlatform`,
+    delete: `${serverUrl}/thirdPartyPlatform/`,
+    generateAPIKey: `${serverUrl}/thirdPartyPlatform/generateAPIKey`,
+  },
+  user: {
+    login: `${serverUrl}/user/login`,
+    query: `${serverUrl}/user/list`,
+    add: `${serverUrl}/user/add`,
+    queryDetail: `${serverUrl}/user/`,
+    update: `${serverUrl}/user`,
+    delete: `${serverUrl}/user/`,
+    connection: `${serverUrl}/user/connection`,
+  },
+  interface: {
+    query: `${serverUrl}/external/interface/list`,
+    add: `${serverUrl}/external/interface/add`,
+    queryDetail: `${serverUrl}/external/interface/`,
+    update: `${serverUrl}/external/interface`,
+    delete: `${serverUrl}/external/interface/`,
+    queryDBs: `${serverUrl}/external/listDatabases/`,
+    queryDBTables: `${serverUrl}/external/listTables/`,
+    queryDBFileds: `${serverUrl}/external/getTableSchema/`,
+    publish: `${serverUrl}external/interface/publish/`,
+  },
 };
 export default Config;
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,26 +1,53 @@
-import React, { Component, Suspense, lazy } from 'react';
-import ReactDOM from 'react-dom';
-import Loading from '../helper/loading.js'
+import React, { Component, Suspense, lazy } from "react";
+import ReactDOM from "react-dom";
+import Loading from "../helper/loading.js";
 
-import { HashRouter as Router, withRouter,Switch,Route } from 'react-router-dom';
+import {
+  HashRouter as Router,
+  withRouter,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
-const Interface = lazy(() => import('./interface/Index'))
-const Source = lazy(() => import('./source/Index'))
-const Third = lazy(() => import('./system/Third'))
-const User = lazy(() => import('./system/User'))
+const Login = lazy(() => import("../Login"));
+const Interface = lazy(() => import("./interface/Index"));
+const Source = lazy(() => import("./source/Index"));
+const Third = lazy(() => import("./system/Third"));
+const User = lazy(() => import("./system/User"));
 
+const requireAuth = (nextState, replace, next) => {
+  // 切换路由时初始化环境
+  if (process.env.NODE_ENV === "development") {
+    // 本地调试环境不进行auth
+    next();
+    return;
+  } else {
+    // next();
+    // return;
+    let dbs_token = sessionStorage.getItem("dbsys_token");
+    if (dbs_token) {
+      next();
+      return;
+    } else {
+      //跳转登录
+      this.router.push("/login");
+    }
+    // sessionStorage.setItem("dbsys_token", "value");
+  }
+};
 const HomeApp = () => (
-    <Router>
+  <Router>
     <Suspense fallback={<Loading />}>
-    <Switch>
-      <Route exact path='/' component={Interface}/>
-      <Route exact path='/interface' component={Interface}/>
-      <Route exact path='/source' component={Source}/>
-      <Route exact path='/third' component={Third}/>
-      <Route exact path='/user' component={User}/>
-      
-    </Switch>
-    {/* <Switch>
+      <Switch>
+        <Route exact path="/" component={Interface} onEnter={requireAuth} />
+        <Route exact path="/interface" component={Interface} />
+        <Route exact path="/source" component={Source} />
+        <Route exact path="/third" component={Third} />
+        <Route exact path="/user" component={User} />
+        <Route exact path="/login" component={Login} />
+      </Switch>
+      {/* <Switch>
       <Route exact path='/develop/taskmanage' component={TaskManagedevelop}/>
       <Route exact path='/develop/detail/:id' component={Detaildevelop}/>
       <Route exact path='/develop/addtask/:id' component={Adddevelop}/>
